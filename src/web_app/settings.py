24834ebf11c0c79f334e74a275c32eb82775a76f
unknown <dario.patt2@gmail.com>
2021-10-19 23:48:52 +0200

    added static file docker system

--- src/web_app/settings.py
+++ src/web_app/settings.py
@@ -148,10 +148,14 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
-
-TEMP = os.path.join(BASE_DIR, "media_cdn/temp")
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
+    TEMP = os.path.join(BASE_DIR, "media_cdn/temp")
+else:
+    STATIC_ROOT = "/vol/static/static"
+    MEDIA_ROOT = "/vol/static/media"
+    TEMP = "/vol/static/temp"
 
 BASE_URL = "http://127.0.0.1:8000"
 
